a6e1c76283d918230bd5c8aaf8f6f02197582ad1
nikita <khlestovnikita@gmail.com>
2014-07-30 22:55:03 +0300

    added technical app and command to change site name and site domain

---
+++ project_name/apps/technical/management/commands/updatedefaultsite.py
@@ -0,0 +1,69 @@
+"""
+Management utility to create or update default site.
+based on
+https://github.com/peiwei/pinax/blob/master/pinax/apps/site_default/management/commands/createdefaultsite.py
+"""
+
+import re
+from optparse import make_option
+
+from django.contrib.sites.models import Site
+from django.core import exceptions
+from django.core.management.base import BaseCommand, CommandError
+from django.utils.translation import ugettext as _
+
+
+DOMAIN_RE = re.compile(
+    r'(?:[A-Z0-9-]+\.)+[A-Z]{2,6}$', re.IGNORECASE)  # domain
+
+
+def is_valid_domainname(value):
+    if not DOMAIN_RE.search(value):
+        raise exceptions.ValidationError(_('Enter a valid domain name.'))
+
+
+def is_valid_displayname(value):
+    pass
+
+
+class Command(BaseCommand):
+    option_list = BaseCommand.option_list + (
+        make_option('--sitedomainname', dest='domainname', default=None,
+            help='Specifies the site domain name.'),
+        make_option('--sitedisplayname', dest='displayname', default=None,
+            help='Specifies the site display name.'),
+    )
+    help = 'Used to create a default site. Provide both --sitedomainname and --sitedisplayname to suppress prompt. Remember double quoting display name if necessary.'
+
+    def handle(self, *args, **options):
+        domainname = options.get('domainname', None)
+        displayname = options.get('displayname', None)
+
+        # We need both arguments to supress prompt.
+        if ( domainname and not displayname ) or ( not domainname and displayname ):
+            raise CommandError("You must use both --sitedomainname and --sitedisplayname.")
+
+        if domainname and displayname:
+            try:
+                is_valid_domainname(domainname)
+            except exceptions.ValidationError:
+                raise CommandError("Invalid domain name.")
+            try:
+                is_valid_displayname(displayname)
+            except exceptions.ValidationError:
+                raise CommandError("Invalid display name.")
+
+        from django.conf import settings
+        sid = getattr(settings.SITE_ID, 1)
+
+        #Use the existing site object, or create one with the SITE_ID.
+        try:
+            site = Site.objects.get(id=sid)
+            site.domain = domainname
+            site.name = displayname
+        except:
+            site = Site(id=sid, domain=domainname, name=displayname)
+
+        #save the newly created, or modified site object
+        site.save()
+        Site.objects.clear_cache()
--- project_name/settings/base.py
+++ project_name/settings/base.py
@@ -142,7 +142,7 @@ TEMPLATE_CONTEXT_PROCESSORS = (
     'django.core.context_processors.tz',
     'django.contrib.messages.context_processors.messages',
     'django.core.context_processors.request',
-    
+
     'constance.context_processors.config',
 )
 
@@ -204,6 +204,9 @@ DJANGO_APPS = (
     'constance',
     'constance.backends.database',
     'storages',
+
+    # Technical app (to add management commands)
+    'technical',
 )
 
 # DEBUG-specific apps
